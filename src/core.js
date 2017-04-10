8d86b825fc2ce80a3bdc94fae5fa2ec204073578
Alex Donley <donleyac@gmail.com>
2017-04-10 16:01:08 -0400

    modifying the collection manipulation function to accomadte for the ability to update location data on collections.

--- src/core.js
+++ src/core.js
@@ -16,31 +16,70 @@ export function modIndicator(state, playerId, label, value, op){
   else {
     return state.updateIn(
       ["playersById",playerId, 'indicators', label],
-      0,
+      // 0,
       indicator => indicator + value
     );
   }
 }
 export function modCollection(state, collection, property, value, op, category){
+
+  let remove = function(list, category )
+
   //content is added or removed from a list 1-by-one
   if (property ==="content"){
-    //Stacked has different content structure than free
+    // Stacked has different content structure than free
     if(state.getIn(["collections",collection,"layout"])==="stacked"){
-      return state.updateIn(
-        ["collections", collection , property],
-        0,
-        (op==="add")? content => content.push(value): content => content.filter(elem => elem!=value)
-      );
+      if(op==="add") {
+        return state.updateIn(
+          ["collections", collection , property],
+          0,
+          content => content.push(value)
+        );
+      }
+      else if(op==="rm"){
+        return state.updateIn(
+          ["collections", collection , property],
+          0,
+          content => content.filter(elem => elem!=value)
+        );
+      }
+      //replace
+      else {
+        return state.updateIn(
+          ["collections", collection , property],
+          0,
+          content => value
+        );
+      }
     }
+    //not stacked
     else {
-      return state.updateIn(
-        ["collections", collection , property],
-        0,
-        (op==="add")? content => content.push(value): content => content.filter(row => (row.get(0)!=category || row.get(1)!=value))
-      );
+      if(op==="add"){
+        return state.updateIn(
+          ["collections", collection , property],
+          0,
+          content => content.push(value)
+        );
+      }
+      //Todo fix so that it doesnt remove all references to that element
+      else if(op==="rm"){
+        return state.updateIn(
+          ["collections", collection , property],
+          0,
+          content => content.filter(row => (row.get(0)!=category || row.get(1)!=value))
+        );
+      }
+      //replace
+      else {
+        return state.updateIn(
+          ["collections", collection , property],
+          0,
+          content => value
+        );
+      }
     }
   }
-  //visibility and control is a ui-checkbox and returns a new list
+  //If not content, simply replace array
   return state.updateIn(
     ["collections", collection , property],
     0,
