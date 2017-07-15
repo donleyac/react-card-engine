2627fbee5bad259a9033e7ea5480182de8ab3ac0
Alex Donley <donleyac@gmail.com>
2017-07-15 14:38:52 -0400

    Updated readme and added comments to server.js

--- README.md
+++ README.md
@@ -1,7 +1,16 @@
-The goal is to be able to play more singleplayer/multiplayer card games within this. You can customize a variety of attributes and play with others in real-time.
+Under Active development, NOT in playable state
 
-The Goal
-- React Redux
-- Socket.io
-- React Components
-- Sass
+__GOAL__: Play singleplayer/multiplayer card games all you need to do is load a config file.
+
+__RELEASE__: Playable release will be versioned 1.0
+
+__GETTING STARTED__:
+
+```npm install```
+
+```npm run start```
+
+Socket.io server will launch and listen on port 8090.
+
+__REQUIREMENTS__:
+react-card-client
--- src/server.js
+++ src/server.js
@@ -2,9 +2,14 @@ import Server from 'socket.io';
 
 export function startServer(store) {
   const io = new Server().attach(8090);
+  //.subscribe called when state tree is modified by action
+  //io.emit sends state to all clients
   store.subscribe(
     () => io.emit('state', store.getState().toJS())
   );
+  //io.on('connection', socket..: action on new conn.
+  //socket.emit(..: send state to socket(client)
+  //socket.on('action',..: bind server store on client action
   io.on('connection', (socket) => {
     socket.emit('state', store.getState().toJS());
     socket.on('action', store.dispatch.bind(store));
